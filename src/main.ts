a6654f7c166c3941907e3ee3df6f6e9eea354b70
david <dbour.developer@gmail.com>
2021-12-16 10:53:17 -0800

    PR Title Validator - Version 1

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import { isValidTitle } from './validator';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    const input: string = core.getInput('pattern')
+    if (!isValidTitle(github.context.payload.pull_request?.title, input)) {
+      throw new Error('Pull request title did not match the provided pattern.')
+    }
+  } catch (error: any) {
+    core.setFailed(error.message)
   }
 }
 
---
+++ src/validator.ts
@@ -0,0 +1,4 @@
+export function isValidTitle(input: string, pattern: string): boolean {
+    const regexPat = new RegExp(pattern);
+    return input.search(regexPat) !== -1;
+}
